5780205ba98d93f1a0d361b43bc63e67b55e461b
Abid Ali <143890772+abiidali@users.noreply.github.com>
2024-03-22 17:38:01 +0500

    Add files via upload

---
+++ Chap 17 to 18/app.js
@@ -0,0 +1,76 @@
+// Q.1
+// let multidimensionalArray = [];
+
+// multidimensionalArray.push([]);
+// multidimensionalArray.push([]);
+
+// console.log(multidimensionalArray);  Output: [[], []]
+
+// Q.2
+
+// let matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]];
+
+// console.log(matrix);
+
+// Q.3
+// for(var i = 1; i<=10; i++ ){
+// console.log(i)
+// }
+
+// Q.4
+// var tableNumber = +prompt("Enter table number")
+//  var tableLen = +prompt("Enter table length")
+
+// for ( var i=1; i <=tableLen; i++) {
+//          document.write( tableNumber + " x" + i +  "=" + tableNumber * i + "</br>")
+//      }
+
+// Q.5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// console.log("Printing items using for loop:");
+// for (var i = 0; i <=4; i++) {
+//     console.log(fruits[i]);
+// }
+
+// Q.6
+
+// Counting
+
+// for(var i=1 ; i<=10 ; i++ ){
+//     console.log(i)
+// }
+
+// reverse Counting
+
+// for(var i=10 ; i >= 1 ; i-- ){
+//     console.log(i)
+// }
+
+// odd series
+
+// for(var i =1 ; i<=19 ; i++){
+//     if (i % 2 ==1){
+//                   console.log (i)
+//               }
+//           }
+
+// even series
+
+// for(var i =1 ; i<=20 ; i++){
+//    if (i % 2 ==0){
+//      console.log (i)
+//    }
+//   }
+
+// series
+
+//     for(var i=2 ; i <=20 ; i++ ){
+//  if( i  <20 + "k" ){
+//     // i +="k";
+//  }
+//         console.log(i+"k" )
+// }
